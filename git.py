59c0203ceae0688961f1327216f17e2f56ae171e
TakTail <stimik56694@gmail.com>
2022-11-23 09:51:14 +0100

    request demo

--- git.py
+++ git.py
@@ -1,1 +1,12 @@
-print("hello world")
+import requests
+
+website = requests.get('http://x.com')
+
+
+if website.status_code == 200:
+    print('Success!')
+    print(website.text)
+elif website.status_code == 404:
+    print('Not Found.')
+else:
+    print("Unknown Error!")
