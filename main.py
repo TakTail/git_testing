6291712ab46aed3f251651e35d121eb4472f8b99
TakTail <stimik56694@gmail.com>
2022-11-23 10:14:21 +0100

    added response codes

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ website = requests.get('http://x.com')
 
 
 if website.status_code == 200:
-    print('Success!')
+    print(f"code={website.status_code} | Success!")
     print(website.text)
 elif website.status_code == 404:
-    print('Not Found.')
+    print(f"code={website.status_code} | Not Found.")
 else:
-    print("Unknown Error!")
+    print(f"code={website.status_code} | Unknown Error!")
