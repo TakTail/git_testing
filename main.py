ae3f4418de58a47a654bef0bf9357a4e2db614e5
TakTail <stimik56694@gmail.com>
2022-11-23 10:16:23 +0100

    test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ website = requests.get('http://x.com')
 
 if website.status_code == 200:
     print(f"code={website.status_code} | Success!")
-    print(website.text)
+    print(f"website content: {website.text}")
 elif website.status_code == 404:
     print(f"code={website.status_code} | Not Found.")
 else:
